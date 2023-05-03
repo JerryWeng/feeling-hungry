341014af939df72b383c05255a061a9d69d5932b
Kennette Maddela <kennettemaddela@Kennettes-MacBook-Pro.local>
2023-05-03 19:12:57 -0400

    Implement simple link designs:

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,28 @@
-import Register from './components/pages/Register.jsx'
-import Navbar from './components/Navbar.jsx'
-import Home from './components/pages/Home.jsx'
-import Social from './components/pages/Social.jsx'
-import Map from './components/Map.jsx'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Register from "./components/pages/Register.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./components/pages/Home.jsx";
+import Social from "./components/pages/Social.jsx";
+import Map from "./components/Map.jsx";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
-
-  return (
-      <BrowserRouter>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/Social">Social</Link>
-        </div>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/social' element={<Social />} />
-          <Route exact path='/register' element={<Register />} />
-        </Routes>
-      </BrowserRouter>
-
-  );
-}
+	return (
+		<BrowserRouter>
+			<div className="bg-blue-900 space-x-10 px-2 py-4">
+				<Link to="/" className="bg-black text-white px-4 py-2 rounded-md">
+					Home
+				</Link>
+				<Link to="/Social" className="bg-black text-white px-4 py-2 rounded-md">
+					Social
+				</Link>
+			</div>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route exact path="/social" element={<Social />} />
+				<Route exact path="/register" element={<Register />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
 export default App;
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,29 +1,44 @@
-import Register from '../pages/Register.jsx'
-import React, { useState } from "react"
-import LoginTransition from "./LoginTransition.jsx"
-import styles from './login.module.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Register from "../pages/Register.jsx";
+import React, { useState } from "react";
+import LoginTransition from "./LoginTransition.jsx";
+import styles from "./login.module.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const Login = () => {
-    const [show, setShow] = useState(false)
-    const [email, setEmail] = useState('');
-    const [pass, setPass]  = useState('');
+	const [show, setShow] = useState(false);
+	const [email, setEmail] = useState("");
+	const [pass, setPass] = useState("");
 
-    return (
-        <div>
-        <button className={styles.loginButton} onClick= {() => setShow(true) }>Login</button>
-        <LoginTransition onClose={() => setShow(false)} show={show}>
-            <div className={styles.loginInformation}> 
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" id="email" name="email"/>
-                <label htmlFor="password">password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Enter password" id="password" name="password"/>
-                <Link to="/Register">Don't have an account?</Link>
-            </div>
-        </LoginTransition>
-        </div>
-    )
-}
+	return (
+		<div>
+			<button className={styles.loginButton} onClick={() => setShow(true)}>
+				Login
+			</button>
+			<LoginTransition onClose={() => setShow(false)} show={show}>
+				<div className={styles.loginInformation}>
+					<label htmlFor="email">email</label>
+					<input
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						type="email"
+						placeholder="Enter email"
+						id="email"
+						name="email"
+					/>
+					<label htmlFor="password">password</label>
+					<input
+						value={pass}
+						onChange={(e) => setPass(e.target.value)}
+						type="password"
+						placeholder="Enter password"
+						id="password"
+						name="password"
+					/>
+					<Link to="/Register">Don't have an account?</Link>
+				</div>
+			</LoginTransition>
+		</div>
+	);
+};
 
-export default Login
-
+export default Login;
