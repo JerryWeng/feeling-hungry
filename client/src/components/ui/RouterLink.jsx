bd81036c7367dc9ef1e2296370ef10e75f7bca02
Kennette Maddela <kennettemaddela@Kennettes-MacBook-Pro.local>
2023-05-03 19:18:30 -0400

    Add hover styles to links:

--- client/src/components/ui/RouterLink.jsx
+++ client/src/components/ui/RouterLink.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 
 const RouterLink = ({ linkText, to }) => {
 	return (
-		<Link to={to} className="bg-black text-white px-4 py-2 rounded-md">
+		<Link
+			to={to}
+			className="bg-black text-white px-4 py-2 rounded-md hover:scale-105 duration-200"
+		>
 			{linkText}
 		</Link>
 	);
