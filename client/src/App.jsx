c3123715f1bdcc9cd389df2565e82c75e03d4d63
Kennette Maddela <kennettemaddela@Kennettes-MacBook-Pro.local>
2023-05-03 19:25:58 -0400

    Abstract browser router to iroot index file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,20 @@
 import Register from "./components/pages/Register.jsx";
 import Home from "./components/pages/Home.jsx";
 import Social from "./components/pages/Social.jsx";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/layout/Header";
 
 const App = () => {
 	return (
-		<BrowserRouter>
+		<div>
 			<Header />
-
 			<Routes>
 				<Route exact path="/" element={<Home />} />
 				<Route exact path="/social" element={<Social />} />
 				<Route exact path="/register" element={<Register />} />
 			</Routes>
-		</BrowserRouter>
+		</div>
 	);
 };
 
