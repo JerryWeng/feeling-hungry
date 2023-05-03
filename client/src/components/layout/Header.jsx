ff3d7d0bb7cabfa90f8633b2a420cf64b82af552
Kennette Maddela <kennettemaddela@Kennettes-MacBook-Pro.local>
2023-05-03 19:33:34 -0400

    Implement searchbar

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 import RouterLink from "../ui/RouterLink";
+import SearchBar from "../ui/SearchBar";
 
 const Header = () => {
 	return (
-		<div className="bg-blue-900 space-x-5 px-2 py-4">
-			<RouterLink linkText="Home" to="/" />
-			<RouterLink linkText="Social" to="/social" />
+		<div className="bg-blue-900 flex justify-between items-center px-2 py-4">
+			<div className="space-x-5">
+				<RouterLink linkText="Home" to="/" />
+				<RouterLink linkText="Social" to="/social" />
+			</div>
+			<SearchBar />
 		</div>
 	);
 };
