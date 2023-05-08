1f7908bfc501b040471bd6de245052053a1c1ae5
Jerry <mcjerry168@gmail.com>
2023-05-08 02:26:35 -0400

    added the map and current location

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,19 @@
+import { useLoadScript } from "@react-google-maps/api";
+
 import Header from "../components/layout/Header.jsx";
-import Map from "../components/Map.jsx";
+import Map from "../components/map/Map.jsx";
 
 const Home = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"]
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+
   return (
-    // <div id="map"></div>
     <div>
-      <div>{/* <Header /> */}</div>
-      <div>{/* <Map /> */}</div>
+      <Map />
     </div>
   );
 };
